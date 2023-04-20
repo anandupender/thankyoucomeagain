687145b0f9a022a8a381b2ecb6e8b23dd5345486
anandupender <anand.upender@gmail.com>
2023-04-20 09:20:32 -0700

    small change

--- static/js/main.js
+++ static/js/main.js
@@ -9,4 +9,24 @@ window.addEventListener("DOMContentLoaded", (event) => {
     function displayContact(){
         phone.style.opacity = "1";
     }
+
+    const galleryContainer = document.querySelector("#gallerySwap");
+    let images = galleryContainer.children;
+    window.setInterval(swapImage, 300);
+    var i = 0;
+
+    function swapImage(){
+        if(i > 0){
+            images[i-1].style.display = "none";
+        }else{
+            images[images.length - 1].style.display = "none";
+        }
+        images[i].style.display = "block";
+        i++;
+        if(i >= images.length){
+            i = 0;
+        }
+    }
+
+
 });
